f696ca17faba9ac57d3e78a797ec1134acf34eb1
Dileep Singh Kushwaha <dileepsinghkushwaha16@gmail.com>
2017-12-11 22:27:22 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#experiment for digital noise generation
+
+#b = np.linspace(1,10,100)
+#generate a digital signal
+a = np.random.randn(500)
+plt.plot(a)
+plt.show()
+
